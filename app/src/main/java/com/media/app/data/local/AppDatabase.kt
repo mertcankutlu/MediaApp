9e55b7541aa2dbd78dbb09c9425f8364cfa400d2
mertcankutlu <mertck04@gmail.com>
2026-08-27 21:48:28 +0300

    Create AppDatabase.kt

---
+++ app/src/main/java/com/media/app/data/local/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.media.app.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [MediaEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun mediaDao(): MediaDao
+}
