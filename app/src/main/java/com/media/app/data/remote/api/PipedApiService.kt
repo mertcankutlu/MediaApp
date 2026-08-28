d550ffa47e573abb17c4c92a7dd54092865040e1
mertcankutlu <mertck04@gmail.com>
2026-08-28 13:55:26 +0300

    Update PipedApiService.kt

--- app/src/main/java/com/media/app/data/remote/api/PipedApiService.kt
+++ app/src/main/java/com/media/app/data/remote/api/PipedApiService.kt
@@ -4,17 +4,20 @@ import com.media.app.data.remote.dto.PipedSearchResultDto
 import com.media.app.data.remote.dto.PipedStreamResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface PipedApiService {
 
-    @GET("search")
+    @GET
     suspend fun search(
+        @Url fullUrl: String,
         @Query("q") query: String,
         @Query("filter") filter: String = "music_songs"
     ): List<PipedSearchResultDto>
 
-    @GET("streams")
+    @GET
     suspend fun getStreams(
+        @Url fullUrl: String,
         @Query("v") videoId: String
     ): PipedStreamResponseDto
 }
