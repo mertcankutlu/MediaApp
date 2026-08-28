ab9444d239277918b280ebbb8bd4be6bcf964751
mertcankutlu <mertck04@gmail.com>
2026-08-28 09:06:28 +0300

    Create NetworkModule.kt

---
+++ app/src/main/java/com/media/app/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.media.app.di
+
+import com.media.app.data.remote.api.PipedApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val PIPED_BASE_URL = "https://pipedapi.kavin.rocks/"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePipedApiService(okHttpClient: OkHttpClient): PipedApiService {
+        return Retrofit.Builder()
+            .baseUrl(PIPED_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PipedApiService::class.java)
+    }
+}
